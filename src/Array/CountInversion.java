8de20fb52b090a450ff8def980ab19f0fe60d8b0
Thanirmalai <2003thanir10@gmail.com>
2023-08-24 20:26:33 +0800

    Count Inversion Problem

---
+++ src/Array/CountInversion.java
@@ -0,0 +1,56 @@
+// Given an array , find the count that i< j and arr[i] > arr[j]
+// Initial approach is nested loop to iterate to find every occurance of the right of each element 
+// But the more optimized solution uses merge sort and while merging two arrays in 
+
+public class CountInversion {
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 1 };
+        numberOfInversions(arr, arr.length);
+        System.out.println(count);
+    }
+
+    public static void numberOfInversions(int[] a, int n) {
+        mergeSort(a, 0, n - 1);
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (right <= left) {
+            return;
+        }
+        int middle = (right + left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        mergeArray(arr, left, middle + 1, right);
+    }
+
+    public static void mergeArray(int[] arr, int left, int middle, int right) {
+        // left -> start of the first array
+        // middle - 1 -> end of the first array
+        // middle -> start of the second array
+        // right -> end of second array
+        int[] mergedArray = new int[right - left + 1];
+        int k = 0;
+        int i = left;
+        int j = middle;
+        while (i < middle && j <= right) {
+            if (arr[i] <= arr[j]) {
+                mergedArray[k++] = arr[i++];
+            } else {
+                mergedArray[k++] = arr[j++];
+                count += (middle - i);
+            }
+        }
+        while (i < middle) {
+            mergedArray[k++] = arr[i++];
+        }
+        while (j <= right) {
+            mergedArray[k++] = arr[j++];
+        }
+        int ptr = left;
+        for (int a = 0; a < mergedArray.length; a++) {
+            arr[ptr++] = mergedArray[a];
+        }
+    }
+}
